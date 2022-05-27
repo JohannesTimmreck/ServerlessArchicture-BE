75bbbc4544cdb6556b87166f919c0c27ee151282
darkelle <djilani.cardineau@epitech.eu>
2022-05-27 23:29:45 +0200

    fix auth

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -1,4 +1,41 @@
-import {firestore} from "firebase-admin";
+import { firestore } from "firebase-admin";
+
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+export async function verifyToken(
+    request: functions.Request
+): Promise<boolean> {
+    try {
+        const token: string | undefined = await getToken(request);
+
+        if (!token) {
+            return false;
+        }
+
+        const payload: admin.auth.DecodedIdToken =
+            await admin.auth().verifyIdToken(token);
+        if (payload !== null) {
+            (<any>request).user = payload
+            return true
+        }
+        return false;
+    } catch (err) {
+        return false;
+    }
+}
+
+async function getToken(request: functions.Request):
+    Promise<string | undefined> {
+    if (!request.headers.authorization) {
+        return undefined;
+    }
+
+    const token: string =
+        request.headers.authorization.replace(/^Bearer\s/, '');
+
+    return token;
+}
 
 export async function isConnectedMiddleware(
     req: any,
@@ -6,19 +43,19 @@ export async function isConnectedMiddleware(
     next: any,
     db: firestore.Firestore
 ) {
-    if (await isConnected(req, db)) {
+    if (await verifyToken(req) && await haveRight(req.user.uuid, db)) {
         next();
     } else {
-        res.status(401).send("Something broke!");
+        res.status(401).send("Need to be auth.");
     }
 }
 
-export async function isConnected(
-    request: any,
+export async function haveRight(
+    uuid: any,
     db: firestore.Firestore
 ): Promise<boolean> {
-    if (request.auth && request.auth.uid) {
-        const user = await db.collection("Users").doc(request.auth.uid).get();
+    if (uuid) {
+        const user = await db.collection("Users").doc(uuid).get();
         return (
             user &&
             user.exists &&

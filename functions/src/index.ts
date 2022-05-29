e4a49dc7b2ca49f973c1980be3e2c9db5bab3ced
darkelle <djilani.cardineau@epitech.eu>
2022-05-29 21:50:09 +0200

    fix cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,9 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { initChatsRoutes } from "./chats";
-import { initMessagesRoutes } from "./messages";
+import {initChatsRoutes} from "./chats";
+import {initMessagesRoutes} from "./messages";
+import * as cors from "cors";
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -15,13 +16,17 @@ const main = express();
 main.use("/v1", app);
 main.use(bodyParser.json());
 
+app.use(cors({
+    origin: "*",
+}));
+
 export const webApiEu = functions.region("europe-west1").https.onRequest(main);
 
 initChatsRoutes(app, db, storage);
 initMessagesRoutes(app, db, storage);
 
 app.use((req: any, res: any) => {
-    res.status(404).json({ message: "This route doesn't exist." });
+    res.status(404).json({message: "This route doesn't exist."});
     res.statusMessage = "This route doesn't exist.";
 });
 
